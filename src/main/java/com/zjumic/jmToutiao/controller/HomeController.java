eacfd10ff4a5ba77075fd6306904e4ca96558ee8
MageMic <MageMic@users.noreply.github.com>
2018-11-17 23:54:45 +0800

    version on 11/16 22:42

--- src/main/java/com/zjumic/jmToutiao/controller/HomeController.java
+++ src/main/java/com/zjumic/jmToutiao/controller/HomeController.java
@@ -1,14 +1,19 @@
 package com.zjumic.jmToutiao.controller;
 
+import com.zjumic.jmToutiao.model.News;
+import com.zjumic.jmToutiao.model.ViewObject;
 import com.zjumic.jmToutiao.service.NewsService;
 import com.zjumic.jmToutiao.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -19,7 +24,19 @@ public class HomeController {
     UserService userService;
 
     @RequestMapping(path = {"/", "/index"}, method = {RequestMethod.GET, RequestMethod.POST})
-    public String index(HttpSession session) {
+    public String index(Model model) {
+        List<News> newsList = newsService.getLatestNews(0,0,10);
+
+        List<ViewObject> vos = new ArrayList<>();
+        for(News news: newsList){
+            ViewObject vo = new ViewObject();
+            vo.set("news",news);
+            news.getCreatedDate();
+            vo.set("user", userService.getUser(news.getUserId()));
+            vos.add(vo);
+        }
+
+        model.addAttribute("vos", vos);
         return "home";
     }
 
