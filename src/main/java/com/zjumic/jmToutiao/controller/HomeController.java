6165916c4d680dadf10e0bc8865de727c2af7469
MageMic <MageMic@users.noreply.github.com>
2018-11-18 16:06:52 +0800

    version on 11/16 22:42

--- src/main/java/com/zjumic/jmToutiao/controller/HomeController.java
+++ src/main/java/com/zjumic/jmToutiao/controller/HomeController.java
@@ -7,9 +7,7 @@ import com.zjumic.jmToutiao.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
@@ -23,21 +21,33 @@ public class HomeController {
     @Autowired
     UserService userService;
 
-    @RequestMapping(path = {"/", "/index"}, method = {RequestMethod.GET, RequestMethod.POST})
-    public String index(Model model) {
-        List<News> newsList = newsService.getLatestNews(0,0,10);
+    private List<ViewObject> getNews(int userId, int offset, int limit) {
+        List<News> newsList = newsService.getLatestNews(userId, offset, limit);
 
         List<ViewObject> vos = new ArrayList<>();
-        for(News news: newsList){
+        for(News news: newsList) {
             ViewObject vo = new ViewObject();
-            vo.set("news",news);
+            vo.set("news", news);
             news.getCreatedDate();
             vo.set("user", userService.getUser(news.getUserId()));
             vos.add(vo);
         }
+        return vos;
+    }
 
-        model.addAttribute("vos", vos);
+    @RequestMapping(path = {"/", "/index"}, method = {RequestMethod.GET, RequestMethod.POST})
+    public String index(@RequestParam(value = "userId", defaultValue = "0")int userId, Model model) {
+        model.addAttribute("vos",getNews(0,0,10));
         return "home";
     }
 
+    @RequestMapping(path = {"/user/{userId}"}, method = {RequestMethod.GET, RequestMethod.POST})
+    public String userIndex(@PathVariable("userId")int userId, Model model) {
+        model.addAttribute("vos",getNews(userId,0,10));
+        return "home";
+    }
+
+
 }
+
+
