b39d80e16b3d9188e055b27f519422ca119edd2f
MageMic <MageMic@users.noreply.github.com>
2018-11-22 00:28:57 +0800

    version on 11/16 22:42

---
+++ src/main/java/com/zjumic/jmToutiao/interceptor/PassportInterceptor.java
@@ -0,0 +1,59 @@
+package com.zjumic.jmToutiao.interceptor;
+
+import com.zjumic.jmToutiao.dao.LoginTicketDAO;
+import com.zjumic.jmToutiao.dao.UserDAO;
+import com.zjumic.jmToutiao.model.LoginTicket;
+import com.zjumic.jmToutiao.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+
+@Component
+public class PassportInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    private LoginTicket loginTicket;
+    @Autowired
+    private UserDAO userDAO;
+    @Autowired
+    private LoginTicketDAO loginTicketDAO;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String ticket = null;
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                if (cookie.getName().equals("ticket")){
+                    ticket = cookie.getValue();
+                    break;
+                }
+            }
+        }
+        if (ticket != null) {
+            LoginTicket loginTicket = loginTicketDAO.selectByTicket(ticket);
+            if (loginTicket == null || loginTicket.getExpired().before(new Date()))
+                return true;
+        }
+
+        User user = userDAO.selectById(loginTicket.getId());
+
+
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
