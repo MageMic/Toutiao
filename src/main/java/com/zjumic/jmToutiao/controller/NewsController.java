472fc1d943e15292c26cf579b326c78cabfeb779
MageMic <MageMic@users.noreply.github.com>
2018-11-25 23:35:56 +0800

    version on 11/16 22:42

---
+++ src/main/java/com/zjumic/jmToutiao/controller/NewsController.java
@@ -0,0 +1,55 @@
+package com.zjumic.jmToutiao.controller;
+
+import com.zjumic.jmToutiao.service.NewsService;
+import com.zjumic.jmToutiao.util.JiemeiUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+
+@Controller
+public class NewsController {
+    private static final Logger logger = LoggerFactory.getLogger(NewsController.class);
+
+    @Autowired
+    NewsService newsService;
+
+    @RequestMapping(path = {"/image"}, method = {RequestMethod.GET})
+    @ResponseBody
+    public void getImage(@RequestParam("name") String imageName,
+                         HttpServletResponse response) {
+        try {
+            response.setContentType("image/jpeg");
+            StreamUtils.copy(new FileInputStream(new
+                    File(JiemeiUtil.IMAGE_DIR + imageName)),
+                    response.getOutputStream());
+        } catch (Exception e) {
+            logger.error("读取图片错误" + e.getMessage());
+        }
+    }
+
+    @RequestMapping(path = {"/uploadImage/"}, method = {RequestMethod.POST})
+    @ResponseBody
+    public String uploadImage(@RequestParam("file")MultipartFile file) {
+        try {
+            String fileUrl = newsService.saveImage(file);
+            if (fileUrl == null) {
+                return JiemeiUtil.getJSONString(1, "上传图片失败");
+            }
+            return JiemeiUtil.getJSONString(0, fileUrl);
+        } catch (Exception e) {
+            logger.error("上传图片失败" + e.getMessage());
+            return JiemeiUtil.getJSONString(1, "上传失败");
+        }
+    }
+}
